8f13b36d9c873572ee82ae6f8322c180fd158afd
Susan Mariana de Jesús Truyenque Jiménez <marianatruyenque@gmail.com>
2023-08-05 13:36:49 -0500

    mi primer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,48 @@
 const cipher = {
-  // ...
-};
+  encode: function (offset, textArea1) {
+    let resultado = '';
+    if (offset > 0 && typeof textArea1 === 'string') {
 
+      if (textArea1 && textArea1.length > 0) {
+        if (textArea1.match(/[a-z]/)) {
+          alert("Solo se admiten mayúsculas");
+          return; // Detenemos el cifrado si hay minúsculas.
+        }
+
+        for (let i = 0; i < textArea1.length; i++) {
+          const caracterCodigo = textArea1.charCodeAt(i);
+          const cifrado = ((caracterCodigo - 65 + offset) % 26 + 65);
+          resultado += String.fromCharCode(cifrado);
+        }
+      }
+
+      return resultado;
+    }
+    else { throw new TypeError('No valido') }
+  },
+
+  decode: function (offset, textArea2) {
+    let resultado = '';
+    if (offset > 0 && typeof textArea2 === 'string') {
+
+      // hacer un if que si ponemos 0 o null nos salga type error 
+      if (textArea2 && textArea2.length > 0) {
+        if (textArea2.match(/[a-z]/)) {
+          alert("Solo se admiten mayúsculas");
+          return; // Detenemos el descifrado si hay minúsculas.
+        }
+
+        for (let i = 0; i < textArea2.length; i++) {
+          const caracterCodigo = textArea2.charCodeAt(i);
+          const descifrado = ((caracterCodigo - offset + 65) % 26 + 65);
+          resultado += String.fromCharCode(descifrado);
+        }
+      }
+
+      return resultado;
+    }
+    else { throw new TypeError('No valido') }
+  }
+};
 export default cipher;
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const textArea1 = document.getElementById('area1');
+const textArea2 = document.getElementById('area2');
+const offset = document.getElementById('offset');
+const botoncifrar = document.getElementById('cifrar');
+const botondescifrar = document.getElementById('descifrar');
+const botonlimpiar = document.getElementById('limpiar');
+
+// evento click
+botoncifrar.addEventListener('click', capturatexto); // Indentación correcta
+botondescifrar.addEventListener('click', capturacodigo); // Indentación correcta
+botonlimpiar.addEventListener('click', limpiar); // Indentación correcta
+
+// Funciones
+function capturatexto() {
+  const codigomsj =cipher.encode(parseInt(offset.value),textArea1.value); // Indentación correcta
+  textArea2.value =codigomsj; // Indentación correcta
+}
+
+function capturacodigo() {
+  const decodigomsj = cipher.decode(parseInt(offset.value), textArea2.value); // Indentación correcta
+  textArea1.value = decodigomsj; // Indentación correcta
+}
+
+function limpiar() {
+  textArea1.value = ''; // Indentación correcta
+  textArea2.value = ''; // Indentación correcta
+  offset.value = '1'; 
+}
